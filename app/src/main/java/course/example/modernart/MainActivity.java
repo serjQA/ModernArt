e9c0cdb47e1f3b38c8811830bef2882be009e26b
SerhII <sergienkosb@gmail.com>
2015-07-15 23:23:55 +0300

    Ready for submit

--- app/src/main/java/course/example/modernart/MainActivity.java
+++ app/src/main/java/course/example/modernart/MainActivity.java
@@ -1,6 +1,12 @@
 package course.example.modernart;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,29 +16,30 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-    SeekBar seekBar;
-    FrameLayout leftTop, topRight, bottom, centerLeft;
+    SeekBar mSeekBar;
+    FrameLayout mLeftTop, mTopRight, mBottom, mCenterRight;
     int seek;
-    TextView text;
+    public DialogFragment mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Variables initialization
+        mLeftTop = (FrameLayout) findViewById(R.id.leftTop);
+        mTopRight = (FrameLayout) findViewById(R.id.topRight);
+        mBottom = (FrameLayout) findViewById(R.id.bottom);
+        mCenterRight = (FrameLayout) findViewById(R.id.centerRight);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
 
-        leftTop = (FrameLayout)findViewById(R.id.leftTop);
-        topRight = (FrameLayout)findViewById(R.id.topRight);
-        bottom = (FrameLayout)findViewById(R.id.bottom);
-        centerLeft = (FrameLayout)findViewById(R.id.centerLeft);
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
-        text = (TextView) findViewById(R.id.textView);
-
+        //Setting up starting colors
         updateBackground();
 
-        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        mSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
 
     }
 
+    //Listen on seekBar changes
     private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -50,13 +57,14 @@ public class MainActivity extends Activity {
         }
     };
 
+    //Changing frames color
     private void updateBackground() {
-        seek = seekBar.getProgress()*3;
+        seek = mSeekBar.getProgress();
 
-        leftTop.setBackgroundColor(0xff000000 + seek * 0x10000 + 5 * 0x100 + 60);
-        topRight.setBackgroundColor(0xff000000 + seek * 0x10000 + 200 * 0x100 + 13);
-        bottom.setBackgroundColor(0xff000000 + seek * 0x10000 + 160* 0x100 + 220);
-//      centerLeft.setBackgroundColor(0xff000000 + seek * 0x10000 + 141 * 0x100 + 18);
+        mLeftTop.setBackgroundColor(0xff000000 + (255 - seek) * 0x10000 + 70 * 0x100 + 70);
+        mTopRight.setBackgroundColor(0xff000000 + 220 * 0x10000 + (255 - seek) * 0x100 + 220);
+        mBottom.setBackgroundColor(0xff000000 + 150 * 0x10000 + 150 * 0x100 + (255 - seek));
+        mCenterRight.setBackgroundColor(0xff000000 + seek * 0x10000 + 141 * 0x100 + 88);
 
 
     }
@@ -74,13 +82,51 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        text.setText("" + id);
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.more_information) {
+            mDialog = RedirectDialogFragment.newInstance();
+            mDialog.show(getFragmentManager(), "Alert");
         }
+        //noinspection SimplifiableIfStatement
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Our Dialog class
+    public static class RedirectDialogFragment extends DialogFragment {
+        public static RedirectDialogFragment newInstance() {
+            return new RedirectDialogFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage("Inspired by coursera." + "\n" + "Click below to learn more!")
+                    .setCancelable(false).setPositiveButton("Not Now",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    ((MainActivity) getActivity()).runBrowser(false);
+                                }
+                            })
+                    .setNegativeButton("Visit MOMA", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ((MainActivity) getActivity()).runBrowser(true);
+                        }
+                    }).create();
+        }
+    }
+
+    //Opening moma site
+    public void runBrowser(boolean boo){
+
+        if(boo) {
+            Intent runBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.moma.org/m"));
+            startActivity(runBrowser);
+        }
+
+        mDialog.dismiss();
     }
 
 }
