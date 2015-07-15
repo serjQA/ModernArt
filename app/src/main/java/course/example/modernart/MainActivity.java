a3635d7f3147d139263f93847e9c24cfcc648e97
= <sergienkosb@gmail.com>
2015-07-15 17:39:54 +0300

    Make frame with color changing

--- app/src/main/java/course/example/modernart/MainActivity.java
+++ app/src/main/java/course/example/modernart/MainActivity.java
@@ -4,14 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.SeekBar;
 
 
 public class MainActivity extends Activity {
+    SeekBar seekBar;
+    FrameLayout leftTop;
+    int seek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leftTop = (FrameLayout)findViewById(R.id.screen);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+
+        updateBackground();
+
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
+    }
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            updateBackground();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private void updateBackground() {
+        seek = seekBar.getProgress();
+        leftTop.setBackgroundColor(0xff000000 + seek*0x10000 + 125 * 0x100 + 60  );
     }
 
     @Override
@@ -35,4 +70,5 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
